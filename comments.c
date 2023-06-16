86b5a447c1869ac8bc4f61742f57456657dc0ebd
jian899 <mjian528@gmail.com>
2023-06-16 13:39:29 +0800

    add files via upload

---
+++ comments.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+    // This is a comment blah
+    /*
+    This
+    is
+    a
+    multiline
+    comment
+    */
+    printf("I like pizza!\n");
+}
---
+++ escape_sequences.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    /* escape sequence = character combination consisting of a backslash \
+       followed by a letter or combination of digits.
+       They specify actions within a line or string of text.
+       \n = newline
+       \t = tab
+    */
+
+int main(){
+    //printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    printf("\\I like pizza\\ - Abraham Lincoin probably");
+    return 0;
+}
