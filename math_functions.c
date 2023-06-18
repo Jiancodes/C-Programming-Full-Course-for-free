4b4a96d4cec10fa2d73a379857dc8ff503411d19
jian899 <mjian528@gmail.com>
2023-06-18 17:47:36 +0800

    add files via upload

---
+++ math_functions.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+
+    double A = sqrt(9);    // 开根
+    double B = pow(2, 4);
+    int C = round(3.14);
+    int D = ceil(3.14);
+    int E = floor(3.99);
+    double F = fabs(-100);
+    double G = log(3);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
+
+
+
+    // printf("\n%1f", A);
+    // printf("\n%1f", B);
+    // printf("\n%d", C);
+    // printf("\n%d", D);
+    // printf("\n%d", E);
+    // printf("\n%lf", F);
+    // printf("\n%lf", G);
+    // printf("\n%lf", H);
+    // printf("\n%lf", I);
+    printf("\n%lf", J);
+
+    return 0;
+}
