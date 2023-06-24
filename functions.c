6b46dddbf794b3d3819d86e47e6179f1c6b20b66
jian899 <mjian528@gmail.com>
2023-06-24 18:42:21 +0800

    add files via upload

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void birthday()
+{
+    printf("\nhappy birhday to you!");
+    printf("\nhappy birhday to you!");
+    printf("\nhappy birhday dear...YOU!");
+    printf("\nhappy birhday to you!\n");
+}
+int main(void){
+
+    birthday();
+    birthday();
+    birthday();
+    return 0;
+}
